900bf08bcc23d177c9be3d098f13f120c62b4fce
Olusesi Barry <bolexido192@gmail.com>
2023-01-02 18:10:23 +0100

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Apple on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
